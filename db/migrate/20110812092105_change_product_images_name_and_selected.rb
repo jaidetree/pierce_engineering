342f09112ab159b91d8360e09e3407ea53006896
Jay Zawrotny <jayzawrotny@gmail.com>
2011-09-13 19:35:36 -0400

    Set default to 0

--- db/migrate/20110812092105_change_product_images_name_and_selected.rb
+++ db/migrate/20110812092105_change_product_images_name_and_selected.rb
@@ -2,7 +2,7 @@ class ChangeProductImagesNameAndSelected < ActiveRecord::Migration
   def self.up
 	  rename_column :product_images, :caption, :image_caption 
 	  rename_column :product_images, :name, :image_selected
-	  change_column :product_images, :image_selected, :integer, { :default => false, :null => false }
+	  change_column :product_images, :image_selected, :integer, { :default => 0, :null => false }
 	  ProductImage.update_all [ "image_selected = ?", false ]
   end
 
